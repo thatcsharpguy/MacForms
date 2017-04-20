1a953a34c416961533020e2ced2d4f6215ab48c3
Antonio Feregrino <antonio.feregrino@gmail.com>
2017-04-20 08:18:00 -0500

    people in space & iss location

--- MacForms/MacFormsPage.xaml.cs
+++ MacForms/MacFormsPage.xaml.cs
@@ -1,10 +1,14 @@
-﻿using System.Net.Http;
+﻿using System.Linq;
+using System.Net.Http;
+using Newtonsoft.Json;
 using Xamarin.Forms;
 
 namespace MacForms
 {
 	public partial class MacFormsPage : ContentPage
 	{
+		HttpClient _client = new HttpClient();
+
 		public MacFormsPage()
 		{
 			InitializeComponent();
@@ -12,9 +16,14 @@ namespace MacForms
 
 		async void Handle_Clicked(object sender, System.EventArgs e)
 		{
-			HttpClient c = new HttpClient();
-			var p = await c.GetStringAsync("http://api.open-notify.org/astros.json");
-			Text.Text = p;
+			var peopleInSpaceResponse = await _client.GetStringAsync("http://api.open-notify.org/astros.json");
+			var peopleInSpace = JsonConvert.DeserializeObject<PeopleInSpaceResponse>(peopleInSpaceResponse);
+			Text.Text = string.Join(", ", peopleInSpace.People.Select(p => $"{p.Name} ({p.Craft})"));
+
+
+			var issLocationResponse = await _client.GetStringAsync("http://api.open-notify.org/iss-now.json");
+			var issLocation = JsonConvert.DeserializeObject<IssPositionResponse>(issLocationResponse);
+			Text.Text +="\n" +$"{issLocation.Timestamp} : {issLocation.IssPosition.Longitude} {issLocation.IssPosition.Latitude}";
 		}
 	}
 }
---
+++ MacForms/PeopleInSpaceResponse.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace MacForms
+{
+	public class PeopleInSpaceResponse
+	{
+		public string Message { get; set; }
+		public PersonInSpace[] People { get; set; }
+		public int Number { get; set; }
+	}
+
+	public class IssPositionResponse
+	{
+		public string Message { get; set; }
+		public long Timestamp { get; set; }
+
+		[JsonProperty("iss_position")]
+		public IssPosition IssPosition { get; set; }
+
+	}
+
+	//{"iss_position": {"latitude": "-8.7373", "longitude": "-85.5523"}, "message": "success", "timestamp": 1492693292}
+
+	public class IssPosition
+	{
+		public double Latitude { get; set; }
+		public double Longitude { get; set; }
+	}
+
+	public class PersonInSpace
+	{
+		public string Name { get; set; }
+		public string Craft { get; set; }
+	}
+}
